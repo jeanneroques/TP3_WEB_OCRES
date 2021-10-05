a47c1135fc35693331ec9ec962cc6606b5b0bff4
jeanneroques <47629487+jeanneroques@users.noreply.github.com>
2021-10-05 15:19:24 +0200

    1er commit

--- js/script.js
+++ js/script.js
@@ -1,30 +1,34 @@
 
 // Fonction appelée lors du click du bouton
-function start() {
+function start(city) {
+  console.info(city)
+
+  // on crée une méthode pour écrire à la console et lier la city de la console à celle de l'api 
   // Création de l'objet apiWeather
-  const apiWeather = new API_WEATHER();
+  const apiWeather = new API_WEATHER(city);
   // Appel de la fonction fetchTodayForecast
 
   apiWeather
     .fetchTodayForecast()
-    .then(function(response) {
+    .then(function (response) {
       // Récupère la donnée d'une API
-      const data = response.data;
+      const data = response.data.list;
 
-      // On récupère l'information principal
-      const main = data.weather[0].main;
-      const description = data.weather[0].description;
-      const temp = data.main.temp;
-      const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
+      for (let i = 0; i < 4; i++) {
+        // On récupère l'information principal
+        const main = data[i].weather[0].main;
+        const description = data[i].weather[0].description;
+        const temp = data[i].temp.day;
+        const icon = apiWeather.getHTMLElementFromIcon(data[i].weather[0].icon);
 
-      // Modifier le DOM
-      document.getElementById('today-forecast-main').innerHTML = main;
-      document.getElementById('today-forecast-more-info').innerHTML = description;
-      document.getElementById('icon-weather-container').innerHTML = icon;
-      document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
-      
+        // Modifier le DOM
+        document.getElementById(`day${i}-forecast-main`).innerHTML = main;
+        document.getElementById(`day${i}-forecast-more-info`).innerHTML = description;
+        document.getElementById(`day${i}-icon-weather-container`).innerHTML = icon;
+        document.getElementById(`day${i}-forecast-temp`).innerHTML = `${temp}°C`;
+      }
     })
-    .catch(function(error) {
+    .catch(function (error) {
       // Affiche une erreur
       console.error(error);
     });
